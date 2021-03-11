8e2f3c476a848b7b9fad8c6a47f9139090fa77e9
Palasak <pe123450@gmail.com>
2021-03-12 03:13:16 +0700

    Create screen and draw player, enemy on screen

--- Game.py
+++ Game.py
@@ -1,20 +1,61 @@
-import pygame
+import pygame, os, random, time
+from Player.player import Player
+from Enemies.enemy import Enemy
 
-def Game:
+WIDTH = 640
+HEIGHT = 480
+FPS = 60
+
+# define colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+
+# Initialize pygame
+pygame.init()
+pygame.mixer.init()
+pygame.display.set_caption("Space Adventure!")
+
+all_sprites = pygame.sprite.Group()
+enemys = pygame.sprite.Group()
+player = Player()
+all_sprites.add(player)
+for i in range(8):
+    e = Enemy()
+    all_sprites.add(e)
+    enemys.add(e)
+
+class Game:
     def __init__(self):
-        self.width = 640
-        self.height = 480
-        self.screen = pygame.display.set_mode((self.width, self.height))
-        self.enemys = []
-        self.lives = 3
-        self.money = 0
+        self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
     def run(self):
         run = True
-        
+        clock = pygame.time.Clock()
         while run:
+            clock.tick(FPS)
+
+            # main event loop
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     run = False
 
-        pygame.quit()
+            self.draw()
+
+    def draw(self):
+        # Update
+        all_sprites.update()
+
+        # Draw / Render
+        self.screen.fill(BLACK)
+        all_sprites.draw(self.screen)
+
+        # *after* drawing everything, flip the screen
+        pygame.display.flip()
+
+
+if __name__ == "__main__":
+    g = Game()
+    g.run()
--- Player/player.py
+++ Player/player.py
@@ -1,5 +1,28 @@
 import pygame
 
-class Player():
+GREEN = (0, 255, 0)
+
+class Player(pygame.sprite.Sprite):
     def __init__(self):
-        
+        pygame.sprite.Sprite.__init__(self)
+        self.WIDTH = 640
+        self.HEIGHT = 480
+        self.image = pygame.Surface((50, 40))
+        self.image.fill(GREEN)
+        self.rect = self.image.get_rect()
+        self.rect.centerx = self.WIDTH / 2
+        self.rect.bottom = self.HEIGHT - 10
+        self.speedx = 0
+
+    def update(self):
+        self.speedx = 0
+        keystate = pygame.key.get_pressed()
+        if keystate[pygame.K_LEFT]:
+            self.speedx = -8
+        if keystate[pygame.K_RIGHT]:
+            self.speedx = 8
+        self.rect.x += self.speedx
+        if self.rect.right > self.WIDTH:
+            self.rect.right = self.WIDTH
+        if self.rect.left < 0:
+            self.rect.left = 0
