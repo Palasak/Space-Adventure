225dd1bee5e54bb606239ec82f3fa5a0b259dcd0
Palasak <pe123450@gmail.com>
2021-03-13 17:25:39 +0700

    update game.py

--- Game.py
+++ Game.py
@@ -278,7 +278,7 @@ class Explosion(pygame.sprite.Sprite):
 
 def show_go_screen():
     screen.blit(background, background_rect)
-    draw_text(screen, "SHMUP!", 64, WIDTH / 2, HEIGHT / 4)
+    draw_text(screen, "Space Adventure!", 64, WIDTH / 2, HEIGHT / 4)
     draw_text(screen, "Arrow keys move, Space to fire", 22, WIDTH / 2, HEIGHT / 2)
     draw_text(screen, "Press a key to begin", 18, WIDTH / 2, HEIGHT * 3 / 4)
     pygame.display.flip()
